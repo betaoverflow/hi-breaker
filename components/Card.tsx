2acf64609b48980314409dbbe6eb18ede2d7efda
Aabhas <aabhassao0@gmail.com>
2022-02-05 20:16:02 +0530

    Feat: Responsive hero section
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

---
+++ components/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="drop-shadow-lg w-72 h-32 md:w-96 md:h-40 bg-white flex flex-col mx-auto justify-center rounded-xl text-center">
+      <h1 className="font-bold text-xl">Did I do something wrong?</h1>
+      <p className="text-3xl font-bold mt-3 text-red-600">Absolutely</p>
+    </div>
+  );
+};
+
+export default Card;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,20 @@
 import styles from "../styles/Hero.module.scss";
+import Card from "./Card";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex flex-col justify-between py-24 px-16">
-      <div className="my-auto">
-        <p className="text-4xl my-2">Hey Hooman 👋</p>
-        <p className="text-8xl my-3">
+    <div className="h-screen flex flex-col justify-center py-18 md:w-5/6 mx-auto">
+      <div className="my-auto flex flex-col">
+        <p className="text-2xl md:text-4xl my-2 ml-1 font-bold">
+          Hey Hooman 👋
+        </p>
+        <p className="text-3xl md:text-7xl font-semibold mt-16">
           Don't just send <span className={styles.highlight}>Hello</span>
         </p>
-        <p className="text-8xl my-3">
+        <p className="text-3xl md:text-7xl font-semibold mt-4 mb-24">
           Directly come to the point <span className="">.</span>
         </p>
+        <Card />
       </div>
       <div className="self-center">
         <p className={styles.pointer}>👇</p>
