277b427cedfcca36def922dfed20c2e5a935e524
sumana2001 <sumana.basu2001@gmail.com>
2022-02-06 12:52:16 +0530

    Footer fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,15 @@ import styles from "../styles/Home.module.scss";
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-      <footer className={styles.footer}>
-        <a
-          href="http://github.com/betaoverflow"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Betaoverflow
-        </a>
-      </footer>
-    </div>
+    <footer className={styles.footer}>
+      <a
+        href="http://github.com/betaoverflow"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Betaoverflow
+      </a>
+    </footer>
   );
 };
 
