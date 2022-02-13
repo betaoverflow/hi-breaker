9be2d19e7ad9fb671d4656b9724379a7eba857c4
Aabhas Sao <aabhassao0@gmail.com>
2022-02-14 00:00:20 +0530

    feat: responsive changes hero section
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,20 +9,22 @@ const Hero = () => {
           <p className="md:mt-16 text-2xl md:text-5xl my-2 ml-1 font-bold">
             Hey Hooman 👋
           </p>
-          <p className="text-2xl md:text-8xl font-medium mt-8 md:mt-24">
+          <p className="text-2xl md:text-5xl lg:text-6xl font-medium mt-8 md:mt-24">
             Don't just send <span className="text-red-600">Hello</span>
           </p>
-          <p className="text-xl md:text-8xl font-medium mt-4 md:mt-16 mb-24">
+          <p className="text-2xl md:text-5xl lg:text-6xl font-medium mt-4 md:mt-16 mb-24">
             Directly come to the point <span className="">.</span>
           </p>
-          <Card>
-            <h1 className="font-bold text-md md:text-xl">
-              Did I do something wrong?
-            </h1>
-            <p className="text-xl md:text-3xl font-bold mt-3 text-red-600">
-              Absolutely
-            </p>
-          </Card>
+          <div className="w-4/5 md:w-2/5 mx-auto">
+            <Card>
+              <h1 className="font-bold text-md md:text-xl">
+                Did I do something wrong?
+              </h1>
+              <p className="text-xl md:text-3xl font-bold mt-3 text-red-600">
+                Absolutely
+              </p>
+            </Card>
+          </div>
         </div>
         <div className="self-center">
           <p className={styles.pointer}>👇</p>
