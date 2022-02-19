e186e56e33fd7aa6f3cc1efec4b61792fd21f3f3
Aabhas Sao <aabhassao0@gmail.com>
2022-02-19 21:31:37 +0530

    Enhacement: Footer content added 💄

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a
-        href="http://github.com/betaoverflow"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by Betaoverflow
-      </a>
+      <div>
+        Based on the concept{" "}
+        <a href="https://web.archive.org/web/20131127020115/http://www.nohello.com/">
+          No Hello
+        </a>
+      </div>
+      <br />
+      <div>
+        Incase you were unable to understand what all these means, it implies
+        don't just send greetings do add the reason you are texting. With love
+        from <a href="https://github.com/betaoverflow">Betaoverflow</a>
+      </div>
     </footer>
   );
 };
